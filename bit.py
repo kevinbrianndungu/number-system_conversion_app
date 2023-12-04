79651a486eb224aed7e97c588368eee67d539fc0
kevinbrianndungu <kevinbrianndungu@gmail.com>
2023-12-04 23:45:39 +0400

    bit.py

--- bit.py
+++ bit.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
